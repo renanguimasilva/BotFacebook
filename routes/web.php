faec759e47f3829e16f459371f3bfc4ac0dee1fc
renanguimasilva <renanvga1@gmail.com>
2020-01-05 15:51:55 -0300

    Rotas para webhook

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ Route::get('/', function () {
     dd($text);
     return view('welcome');
 });
+
+
+Route::prefix('bot')
+    ->group(function() {
+        Route::get('/webhook', 'BotController@subscribe');
+        Route::post('/webhook', 'BotController@receiveMessage');
+    });
